3d500510434f651ea38bcc5e694f5e11f284708d
Ubuntu <ubuntu@ip-172-31-39-151.ec2.internal>
2020-11-24 07:07:47 +0000

    added in area_and_perimeter.py

---
+++ area_and_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Dahrio Francois
+# Created on: November 2020
+# This program calculates the area and perimeter of a rectangle
+#    with dimensions 6cm x 7cm
+
+
+def main():
+    # this function calculates the area and perimeter
+    print("If a rectangle has the dimensions:")
+    print("6cm x 7cm")
+    print("")
+    print("Area is {}cm^2".format(6*7))
+    print("Perimeter is {}cm".format(2*(6+7)))
+
+
+if __name__ == "__main__":
+    main()
